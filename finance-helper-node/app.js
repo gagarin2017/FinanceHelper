d390fa88aa10960469b5ef113e133c5c25010bde
Yury Gagarin <yury_gagarin2017@mail.ru>
2020-12-07 16:10:36 +0000

    updating Port

--- finance-helper-node/app.js
+++ finance-helper-node/app.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const homepageRoute = require('./routes/home')
+const homepageRoute = require("./routes/home");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(homepageRoute);
 
-app.listen(3210);
+let port =
+  process.env.port === null || process.env.port === ""
+    ? 3210
+    : process.env.port;
+app.listen(port);
